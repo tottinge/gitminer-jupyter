7379b0f58e3ca47ff282d4884bc468e1a84487e3
Tim Ottinger <tottinge@industriallogic.com>
2023-04-07 15:52:57 -0500

    feat: moved more simplifed behavior into gitminer.py

--- gitminer.py
+++ gitminer.py
@@ -1,27 +1,29 @@
 from collections import Counter
 from datetime import datetime
 from itertools import combinations
-from git import Repo
-import networkx as nx
+from typing import NamedTuple
 
-repo = Repo("quizzology")
+import networkx as nx
+from git import Repo, Commit
 
 little_labels = dict(with_labels=True, font_size=7)
 medium_labels = dict(with_labels=True, font_size=7)
 
-repo_graph = nx.Graph()
-for commit in repo.iter_commits():
-    for (left, right) in combinations(commit.stats.files, 2):
-        if (left, right) in repo_graph.edges:
-            data = repo_graph.get_edge_data(left, right)
-            data['count'] = data.get('count', 1) + 1
-        else:
-            repo_graph.add_edge(left, right,
-                                count=1,
-                                timestamp=commit.committed_datetime,
-                                hash=commit.binsha)
 
-from typing import NamedTuple
+def graph_file_to_file(git_repo):
+    graph = nx.Graph()
+    for current_commit in git_repo.iter_commits():
+        file_combos = combinations(current_commit.stats.files, 2)
+        for (left, right) in file_combos:
+            if (left, right) in graph.edges:
+                data = graph.get_edge_data(left, right)
+                data['count'] = data.get('count', 1) + 1
+            else:
+                graph.add_edge(left, right,
+                               count=1,
+                               timestamp=current_commit.committed_datetime,
+                               hash=current_commit.binsha)
+    return graph
 
 
 class CommitNode(NamedTuple):
@@ -30,14 +32,31 @@ class CommitNode(NamedTuple):
     timestamp: datetime
 
 
-commit_graph = nx.DiGraph()
-for commit in repo.iter_commits():
-    item = CommitNode(commit.hexsha, commit.message, commit.committed_datetime)
-    for file in commit.stats.files:
-        commit_graph.add_edge(item, file)
+def graph_commit_to_file(repo):
+    graph = nx.DiGraph()
+    commit: Commit
+    for commit in repo.iter_commits():
+        item = CommitNode(commit.hexsha, commit.message, commit.committed_datetime)
+        for filename in commit.stats.files:
+            graph.add_edge(item, filename)
+    return graph
 
-file_commits = Counter()
-for (commit, file) in commit_graph.edges:
-    file_commits[file] += 1
 
-print(file_commits.most_common()[:10])
+# Organize commit data into structures in nx
+repo = Repo("quizzology")
+repo_graph = graph_file_to_file(repo)
+commit_graph = graph_commit_to_file(repo)
+
+
+# Most commonly committed files
+def count_commits(commit_to_file_graph):
+    return Counter(
+        name
+        for name, _ in commit_to_file_graph.edges
+    )
+
+
+file_commits = count_commits(commit_graph)
+print("Most common commits (top 10):")
+for filename, count in file_commits.most_common()[:10]:
+    print(f'{filename}: {count}')
