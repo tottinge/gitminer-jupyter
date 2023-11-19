f88ac06ab04ff4828e6768a7a08f15765f00c5dd
Perry Reid <wpreid@gmail.com>
2023-11-18 19:58:34 -0600

    feat: implemented most_committed command using pandas

---
+++ most_commited.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+import pandas
+import typer
+
+
+def count_files_in_commits(json_file: str):
+    """
+    This counts things
+    """
+    p = pandas.read_json(json_file)
+    counter = Counter()
+    for files in p["files"]:
+        counter.update(files.keys())
+    goal = 10
+    print(f"TOP {goal} most committed files:")
+    for filename, commits in counter.most_common(goal):
+        print(f"  {commits}: {filename}")
+
+
+if __name__ == '__main__':
+    typer.run(count_files_in_commits)
