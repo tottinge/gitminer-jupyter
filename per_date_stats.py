e3fec24137b19f1c01efe1f835623c1b26621d19
Tim Ottinger <tottinge@industriallogic.com>
2023-11-22 19:17:45 +0100

    feat: alternative impls of the criterion range

--- per_date_stats.py
+++ per_date_stats.py
@@ -13,9 +13,9 @@ def report_commits_per_day(json_file, *, after: datetime = None, before: datetim
     number of commits per day.
     """
     p = pandas.read_json(json_file)
-    filter = conditional_range(after, before)
+    conditional = conditional_range(after, before)
 
-    counts = Counter(d.date() for d in pandas.to_datetime(p['date'], utc=True) if filter(d))
+    counts = Counter(d.date() for d in pandas.to_datetime(p['date'], utc=True) if conditional(d))
 
     print("Commits per day (UTC days)")
     for key, value in counts.items():
@@ -27,10 +27,31 @@ def report_commits_per_day(json_file, *, after: datetime = None, before: datetim
 
 
 if __name__ == '__main__':
-    p = report_commits_per_day('miner.json')
+    commits_counter = report_commits_per_day('miner.json')
 
 
-def conditional_range(early: Optional[datetime], late: Optional[datetime]):
+def if_version_of_conditional_range(low_value: Optional[datetime], high_value: Optional[datetime]):
+    if low_value is None and high_value is None:
+        return lambda x: True
+    if low_value is None and high_value is not None:
+        return lambda x: x < high_value.astimezone()
+    if low_value is not None and high_value is None:
+        return lambda x: x > low_value.astimezone()
+    return lambda x: low_value.astimezone() < x < high_value.astimezone()
+
+
+def dictionary_version(low_value: Optional[datetime], high_value: Optional[datetime]):
+    selector = (low_value is not None, high_value is not None)
+    choices = {
+        (False, False): lambda x: True,
+        (True, False): lambda x: x > low_value.astimezone(),
+        (False, True): lambda x: x < high_value.astimezone(),
+        (True, True): lambda x: low_value.astimezone() < x < high_value.astimezone()
+    }
+    return choices[selector]
+
+
+def matching_conditional_range(early: Optional[datetime], late: Optional[datetime]):
     match (early, late):
         case (None, None):
             return lambda x: True
@@ -40,3 +61,6 @@ def conditional_range(early: Optional[datetime], late: Optional[datetime]):
             return lambda x: x < end_date.astimezone()
         case (start_date, end_date):
             return lambda x: start_date.astimezone() < x < end_date.astimezone()
+
+
+conditional_range = if_version_of_conditional_range
