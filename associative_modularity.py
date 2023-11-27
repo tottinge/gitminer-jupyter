cfd1c2c6661d8ee768ded5c866226aaef5baa130
Tim Ottinger <tottinge@industriallogic.com>
2023-11-27 19:30:13 +0100

    feat: working on design modularity

--- associative_modularity.py
+++ associative_modularity.py
@@ -1,27 +1,37 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from itertools import combinations
 from statistics import mean, stdev
 
 import networkx as nx
 import pandas
 from pandas import DataFrame
+from typing import Generator
 
 
-def list_mega_compiles(history: DataFrame):
-    for (index, (hash, date, message,files)) in history[['hash', 'date', 'message', 'files']].iterrows():
+def list_mega_commits(history: DataFrame):
+    for (index, (hashcode, date, message, files)) in history[['hash', 'date', 'message', 'files']].iterrows():
         if len(files) > 100:
-            print(f"{index}, has {len(files)}, {date}, {message}")
+            yield index, len(files), hashcode, date, message
+
+
+def list_super_connectors(history: DataFrame):
+    graph = create_graph_from_dataframe(history)
+    listed = [(neighbors, filename)
+              for filename, neighbors in graph.degree]
+    biggest_first = sorted(listed, reverse=True)
+    return biggest_first[:20]
 
 
 def associative_groupings(json_file):
-    p: DataFrame = pandas.read_json(json_file)
+    commit_history: DataFrame = pandas.read_json(json_file)
 
-    g: nx.Graph = create_graph(count_combinations(p))
+    print("Mega-commits")
+    for line in list_mega_commits(commit_history):
+        print(line)
 
-    print("pMaintenance Groups:")
-    for each in nx.connected_components(g):
-        if len(each) > 3:
-            print(len(each))
+    print("Super-connectors")
+    for item in list_super_connectors(commit_history):
+        print(item)
 
 
 def count_combinations(p: DataFrame) -> Counter:
@@ -36,7 +46,25 @@ def count_combinations(p: DataFrame) -> Counter:
     )
 
 
-def create_graph(pairings: Counter) -> nx.Graph:
+def relative_strengths(p: DataFrame) -> defaultdict:
+    d = defaultdict(float)
+    for files in p['files']:
+        strength = files and 1.0 / len(files) or 0
+        for pair in combinations(files, 2):
+            d[pair] += strength
+    return d
+
+
+def create_graph_from_dataframe(history: DataFrame):
+    source = []
+    for files in history["files"]:
+        if len(files) > 1:
+            for pair in combinations(files.keys(), 2):
+                source.append(pair)
+    return nx.Graph(source)
+
+
+def create_graph_from_counter(pairings: Counter) -> nx.Graph:
     graph = nx.Graph()
     source = (
         (*pair, count)
