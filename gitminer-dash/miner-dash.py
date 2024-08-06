bb8836f6a7c999828c8ae6cc9b23341ad3198a89
Tim Ottinger <tottinge@industriallogic.com>
2024-08-06 14:35:55 +0100

    feat: added and implemented button to load a rep

--- gitminer-dash/miner-dash.py
+++ gitminer-dash/miner-dash.py
@@ -8,6 +8,9 @@ from dash.exceptions import PreventUpdate
 
 repo: Optional[git.Repo] = None
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+app = Dash()
 
 
 class IDs:
@@ -18,44 +21,46 @@ class IDs:
     LOADED_INDICATOR: str = "repo-loaded-indicator"
 
 
-app = Dash()
+last_clicks = 0
 
-app.layout = html.Div(
-    children=[
-        html.Div(children=f"Starting at {os.getcwd()}"),
-        html.Div(children=[
-            html.Label("Repository to evaluate", htmlFor=IDs.REPO_TEXT_INPUT),
-            dcc.Input(id=IDs.REPO_TEXT_INPUT, type="text", value="../ClassDojo/dojo-behavior-ios",
-                      placeholder="repository path here"),
-            html.P(id=IDs.LOADED_INDICATOR, children=["Not Loaded"]),
-            dcc.Loading(id=IDs.LOADING_VIZ, children=[html.P("not loaded yet")])
-        ])
-    ])
 
 def render_selection():
     @app.callback(
         Output(IDs.LOADED_INDICATOR, "children"),
+        Input(IDs.REPO_SELECT_BTN, "n_clicks"),
         Input(IDs.REPO_TEXT_INPUT, "value")
     )
-    def start_loading(path):
-        if not path:
+    def start_loading(clicks, path):
+        global last_clicks
+        if last_clicks == clicks:
             raise PreventUpdate()
+        last_clicks = clicks
+        logger.warning("Loader method called")
         if os.path.isdir(path):
-            return [path]
+            try:
+                global repo
+                repo = git.Repo(path)
+                return html.Div("Loaded the repo!")
+            except Exception as err:
+                return html.Div(f"Failed to load: {err}")
         return ["oops", f"Maybe {path} is not a directory?"]
 
     return html.Div(children=[
-            html.Label("Repository to evaluate", htmlFor=IDs.REPO_TEXT_INPUT),
-            dcc.Input(id=IDs.REPO_TEXT_INPUT,
-                      type="text",
-                      value="../../ClassDojo/dojo-behavior-ios",
-                      placeholder="repository path here"),
-            html.P(id=IDs.LOADED_INDICATOR, children=["Not Loaded"]),
+        html.Label("Repository to evaluate", htmlFor=IDs.REPO_TEXT_INPUT),
+        dcc.Input(id=IDs.REPO_TEXT_INPUT,
+                  type="text",
+                  value="../../ClassDojo/dojo-behavior-ios",
+                  placeholder="repository path here"),
+        html.Button("Submit", id=IDs.REPO_SELECT_BTN),
+        html.P(id=IDs.LOADED_INDICATOR, children=["Not Loaded"]),
     ])
 
 
-
-
-
 if __name__ == '__main__':
+    app.layout = html.Div(
+        children=[
+            html.Div(children=f"Starting at {os.getcwd()}"),
+            html.Br(),
+            render_selection()
+        ])
     app.run(debug=True)
