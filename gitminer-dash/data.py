e88c270f9edce3da9c269da0708c4a914d56bb72
Tim Ottinger <tottinge@industriallogic.com>
2024-10-07 14:23:49 +0100

    feat: adding data collection for the dash app

--- gitminer-dash/data.py
+++ gitminer-dash/data.py
@@ -1,5 +1,12 @@
-from pandas import DataFrame
+from functools import cache
+
+from git import Repo
+
+# Link this to any local repo, until we can make this
+# a handy-dandy drag-n-drop or dir selection input field
+repository_path = "sample_repo"
 
 
-def get_repo() -> DataFrame:
-    return DataFrame()
+@cache
+def get_repo(path: str) -> Repo:
+    return Repo(path)
