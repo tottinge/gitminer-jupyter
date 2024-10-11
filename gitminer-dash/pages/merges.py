700467ec6a729aeecfdcfaff0f9d64f9745047f3
Tim Ottinger <tottinge@industriallogic.com>
2024-10-11 15:41:55 +0100

    feat(merges): Add a page for the merges graph.

--- gitminer-dash/pages/merges.py
+++ gitminer-dash/pages/merges.py
@@ -1,10 +1,52 @@
+from datetime import datetime, timedelta
+
 import dash
-from dash import html
+import pandas as pd
+import plotly.express as px
+from dash import html, dcc
+from dash.dash_table import DataTable
 
 dash.register_page(__name__, title="Merge Sizes")
 
+
+def prepare_dataframe():
+    import data
+    repo = data.get_repo()
+
+    recent_date = datetime.today().astimezone() - timedelta(days=30)
+    recent_merges = [commit for commit in repo.iter_commits()
+                     if commit.committed_datetime > recent_date
+                     and len(commit.parents) > 1]
+
+    columns = ["hash", "date", "comment", "lines", "files"]
+    source = (
+        (commit.hexsha,
+         commit.committed_datetime.date(),
+         commit.message,
+         commit.stats.total["lines"],
+         commit.stats.total["files"])
+        for commit in recent_merges
+    )
+    result = pd.DataFrame(source, columns=columns).sort_values(by="date")
+    print(result)
+    return result
+
+
 layout = html.Div(
     [
-        html.H2("This is the merges page"),
+        html.H2("Merge Magnitudes"),
+        dcc.Graph(
+            id="merge-graph",
+            figure=px.bar(
+                data := prepare_dataframe(),
+                x="date",
+                y="lines",
+                color="files",
+                hover_name="date",
+                hover_data=["files", "lines", "comment"]
+            )),
+        html.Hr(),
+        html.H2("Raw Data"),
+        DataTable(data=data.to_dict("records"))
     ]
 )
