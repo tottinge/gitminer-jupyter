f4da0c894db01e3c95c625def23ce783f0be4eb6
Perry Reid <wpreid@gmail.com>
2024-10-30 09:21:49 -0500

    feat: created the callbacks to populate the most committed page

--- gitminer-dash/pages/most_committed.py
+++ gitminer-dash/pages/most_committed.py
@@ -1,5 +1,12 @@
-from dash import html, register_page, dcc
+from dash import html, register_page, dcc, callback, Output, Input
 from dash.dash_table import DataTable
+import plotly.express as px
+from pandas import DataFrame
+
+fake_data = [
+    ("common.py", 800),
+    ("rare.py", 45)
+]
 
 register_page(
     module=__name__,  # Where it's found
@@ -10,9 +17,28 @@ layout = html.Div(
     [
         html.H2("Most Often Committed Files"),
         dcc.Dropdown(id='period-dropdown', options=['Last 30 days', 'Last 60 days']),
-        html.Div(id='page-content', children=[]),
+        # html.Div(id='page-content', children=[]),
+        dcc.Graph(id='page-content'),
         html.Hr(),
         html.H2("Source Data"),
-        DataTable()
+        DataTable(id='table-data')
     ]
+
+
 )
+
+@callback(
+    Output('page-content', 'figure'),
+    Input('period-dropdown', 'value')
+)
+def populate_graph(period_input):
+    frame = DataFrame(data=fake_data, columns=['filename', 'count'])
+    return px.bar(data_frame=frame, x='filename', y='count')
+
+@callback(
+    Output('table-data', 'data'),
+    Input('period-dropdown', 'value')
+)
+def populate_table(period_input):
+    frame = DataFrame(data=fake_data, columns=['filename', 'count'])
+    return frame.to_dict('records')
