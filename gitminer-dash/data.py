66ebb3bf4e33b7b9a680210a95fffea82f5976cb
Tim Ottinger <tottinge@industriallogic.com>
2024-10-30 14:27:51 +0000

    feat: Added convenience function to retrieve commits by date.

--- gitminer-dash/data.py
+++ gitminer-dash/data.py
@@ -1,8 +1,10 @@
 import os
 import re
+from collections.abc import Iterable
+from datetime import datetime
 from functools import cache
 
-from git import Repo
+from git import Repo, Commit
 
 # Link this to any local repo, until we can make this
 # a handy-dandy drag-n-drop or dir selection input field
@@ -14,5 +16,12 @@ def get_repo(path: str = repository_path) -> Repo:
     return Repo(path)
 
 
+@cache
 def get_repo_name():
     return re.sub(pattern=r'[_\.-]', repl=' ', string=os.path.split(repository_path)[-1]).title()
+
+
+def commits_in_period(beginning: datetime, ending: datetime) -> Iterable[Commit]:
+    for commit in get_repo().iter_commits():
+        if beginning <= commit.committed_datetime <= ending:
+            yield commit
