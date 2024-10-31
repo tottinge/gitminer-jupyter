8b559bb0ae1f4dda24d878bd4bb0b4ef49c18f9e
Perry Reid <wpreid@gmail.com>
2024-10-31 08:13:44 -0500

    feat: introduced a parameter for the path to the repository

--- gitminer-dash/app.py
+++ gitminer-dash/app.py
@@ -1,6 +1,12 @@
+import sys
+
 from dash import html, dcc, Dash, page_container, page_registry
 
 import data
+
+if len(sys.argv) < 2:
+    print("Usage: app.py <repo_name>")
+    exit(1)
 
 app = Dash(__name__, use_pages=True)
 
--- gitminer-dash/data.py
+++ gitminer-dash/data.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 from collections.abc import Iterable
 from datetime import datetime
 from functools import cache
@@ -8,17 +9,18 @@ from git import Repo, Commit
 
 # Link this to any local repo, until we can make this
 # a handy-dandy drag-n-drop or dir selection input field
-repository_path = "../../ClassDojo/dojo-behavior-ios"
+def repository_path():
+    return sys.argv[1]
 
 
 @cache
-def get_repo(path: str = repository_path) -> Repo:
-    return Repo(path)
+def get_repo() -> Repo:
+    return Repo(repository_path())
 
 
 @cache
 def get_repo_name():
-    return re.sub(pattern=r'[_\.-]', repl=' ', string=os.path.split(repository_path)[-1]).title()
+    return re.sub(pattern=r'[_\.-]', repl=' ', string=os.path.split(repository_path())[-1]).title()
 
 
 def commits_in_period(beginning: datetime, ending: datetime) -> Iterable[Commit]:
