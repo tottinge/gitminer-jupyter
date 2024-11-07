176db1eecc37e5fee09f030c1f17fc8214666e19
Tim Ottinger <tottinge@industriallogic.com>
2024-11-07 21:17:02 +0100

    feat(change_types): don't display empty graph and empty table when there is no data - done by deferring load and manipulating 'display' values.

--- gitminer-dash/pages/change_types.py
+++ gitminer-dash/pages/change_types.py
@@ -1,6 +1,8 @@
+from functools import cache
+
 import pandas as pd
 import plotly.express as px
-from dash import html, dcc, register_page
+from dash import html, dcc, register_page, callback, Output, Input
 from dash.dash_table import DataTable
 
 # Note: PyCharm tags these as invalid imports, but we run
@@ -16,11 +18,10 @@ register_page(
 )
 
 
+@cache
 def prepared_data_frame():
     repo = get_repo()
-
     last_20 = get_most_recent_tags(repo, 20)
-
     if not last_20:
         return pd.DataFrame(columns=['Name', 'Date'])
     # Get the Date, Name, and counters for the most recent commit diffs
@@ -31,19 +32,44 @@ def prepared_data_frame():
 
 layout = html.Div(
     [
-        dcc.Graph(id="local_graph",
-                  figure=px.bar(
-                      data := prepared_data_frame(),
-                      title=f"Change Types and Magnitudes Across Tags",
-                      x="Name",
-                      y=list(change_name.values()),
-                      labels=["Added", "Deleted", "Modified", "Removed"],
-                      hover_name="Name",
-                      hover_data=["Date"],
-                      text_auto='.2s'
-                  )),
+        html.Div(id="id-graph-container",
+                 children=[dcc.Graph(id="local_graph", style={"display": "none"}),
+                           html.P(id="id-no-data-message", children="No data available."),
+                           ],
+                 ),
         html.Hr(),
         html.H2("Source Data"),
-        DataTable(data=data.to_dict('records')),
+        DataTable(id="id-data-table"),
     ]
 )
+
+
+@callback(
+    [
+        Output("local_graph", "figure"),
+        Output("id-data-table", "data"),
+        Output("id-graph-container", "style"),
+        Output("id-no-data-message", "style")
+    ],
+    Input("id-graph-container", "n_clicks")
+)
+def update_graph(_):
+    style_show = {"display": "block"}
+    style_hide = {"display": "none"}
+    data = prepared_data_frame()
+    if data.empty:
+        figure = {"data": []}
+        table_data = []
+        return figure, table_data, style_hide, style_show
+    figure = px.bar(
+        data,
+        title="Change Types and Magnitudes Across Tags",
+        x="Name",
+        y=list(change_name.values()),
+        labels=["Added", "Deleted", "Modified", "Removed"],
+        hover_name="Name",
+        hover_data=["Date"],
+        text_auto='.2s'
+    )
+    table_data = data.to_dict('records')
+    return figure, table_data, style_show, style_hide
